bd93885c39e6533a15381ee531bd9542c1b78eac
Pedro Mendes <me@altart.top>
2024-07-13 15:25:37 -0300

    refactor: Remove not needed bodyguards on advancement event

--- src/main/java/org/altart/telegrambridge/events/GameEvent.java
+++ src/main/java/org/altart/telegrambridge/events/GameEvent.java
@@ -60,19 +60,17 @@ public class GameEvent implements Listener {
     @EventHandler
     public void onAdvancement(PlayerAdvancementDoneEvent event) {
         if (TelegramBridge.config.sendToTelegram && TelegramBridge.config.advancementEvent) {
-            String playerNick = event.getPlayer().getDisplayName();
             String advancementKey = event.getAdvancement().getKey().getKey().replace("/", ".");
             if (advancementKey.startsWith("recipes.")) {
                 return;
             }
+
+            String playerNick = event.getPlayer().getDisplayName();
+
             String advancementText = TranslationRegistry.INSTANCE.translate("advancements." + advancementKey + ".title");
-            if (advancementText == null) {
-                advancementText = advancementKey;
-            }
             String advancementDescription = TranslationRegistry.INSTANCE.translate("advancements." + advancementKey + ".description");
-            if (advancementDescription != null) {
-                advancementText += ": " + advancementDescription;
-            }
+            advancementText += ": " + advancementDescription;
+
             HashMap<String, String> values = new HashMap<>();
             values.put("playername", playerNick);
             values.put("advancement", advancementText);
